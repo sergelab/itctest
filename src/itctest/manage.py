3c1c8931fef1ba2277296b3113b6e98e027d47ea
Sergelab <serge@brpr.ru>
2014-12-08 21:54:36 +0300

    upgrade syncdb command

--- src/itctest/manage.py
+++ src/itctest/manage.py
@@ -22,8 +22,45 @@ manager.add_command('assets', ManageAssets())
 collect.init_script(manager)
 
 
+def drop_all():
+    from sqlalchemy.engine import reflection
+    from sqlalchemy.schema import (
+        MetaData,
+        Table,
+        DropTable,
+        ForeignKeyConstraint,
+        DropConstraint
+    )
+
+    inspector = reflection.Inspector.from_engine(db.engine)
+    metadata = MetaData()
+
+    tbs = []
+    all_fks = []
+
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(
+                ForeignKeyConstraint((), (), name=fk['name'])
+            )
+        t = Table(table_name, metadata, *fks)
+        tbs.append(t)
+        all_fks.extend(fks)
+
+    for fkc in all_fks:
+        db.engine.execute(DropConstraint(fkc))
+
+    for table in tbs:
+        db.engine.execute(DropTable(table))
+
+
 @manager.command
 def syncdb(console=True):
+    drop_all()
+
     db.create_all()
     db.session.commit()
 
